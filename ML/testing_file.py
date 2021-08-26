cc1bbda29c9979c827a143e37b7a4181f1afce67
AlainTiri <alaintiri@gmail.com>
2021-08-26 17:00:21 +0200

    Moyennes ajoutées. Reste Median + save + preprocess

--- ML/testing_file.py
+++ ML/testing_file.py
@@ -6,28 +6,47 @@ data_path = os.path.join(os.path.dirname(__file__), "Data", "database.csv")
 zip_path = os.path.join(os.path.dirname(__file__), "Data", "zipcodes.csv")
 
 
-def count_by_locality(df):
+def zip_trunc(zip_code: int) -> int:
+    rest = zip_code % 10
+    zip_code = zip_code - rest
+    return zip_code
+
+
+def count_by_locality(df, df_zip_code):
+    df_zip_code["zip_trunck"] = df_zip_code.zipcode.apply(zip_trunc)
+    df_zip_code["count_CP"] = 0
+    print(df_zip_code.head())
+
     df_prices = df.groupby("Locality").count()
     s_count = df_prices.Province
 
     new_df = pd.DataFrame(s_count.rename("count_CP", inplace=True))
     new_df.reset_index(level=0, inplace=True)
+    df["zip_trunck"] = df["Locality"].apply(zip_trunc)
     print(new_df)
 
     for row in new_df.itertuples():
-        suite = df[df['Locality'] == row.Locality]
-        moyenne = suite.mean()
+        df_zip_code.loc[df_zip_code["zipcode"] == row.Locality, 'count_CP'] = row.count_CP
+        select_zip = df[df['Locality'] == row.Locality]
+        moyenne = select_zip.mean()
         moyenne = moyenne['Price']
-        print('moyenne:', moyenne)
-        df.loc[df["Locality"] == row.Locality, 'moyenne'] = moyenne
+        new_df.loc[new_df["Locality"] == row.Locality, 'moyenne'] = moyenne
+        df_zip_code.loc[df_zip_code["zip_trunck"] == row.Locality, 'moyenne'] = moyenne
 
-        print(row.Locality, row.count_CP, moyenne)
+    for row in df_zip_code.itertuples():
+        if row.count_CP < 4 :
+            select_zip = df[df['zip_trunck'] == row.zip_trunck]
+            moyenne = select_zip.mean()
+            moyenne = moyenne['Price']
+            new_df.loc[new_df["Locality"] == row.zipcode, 'moyenne'] = moyenne
+            df_zip_code.loc[df_zip_code["zipcode"] == row.zipcode, 'moyenne'] = moyenne
 
-    return df
+    return df_zip_code
 
 
 df: pd.DataFrame = pd.read_csv(data_path, index_col=0)
-zip: pd.DataFrame = pd.read_csv(zip_path, index_col=0)
+zip_code: pd.DataFrame = pd.read_csv(zip_path, index_col=0)
 
-print(df.shape)
-counter = count_by_locality(df, zip)
+print(zip_code.columns)
+counter = count_by_locality(df, zip_code)
+print(counter.head(100))
